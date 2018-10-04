6a132d0450914d33cc2e1a056c093734d7433dea
Alexey Ermolaev <dotterian@dotterian.ru>
2018-10-04 22:01:20 +0300

    Фикс строгой типизации

--- src/layout/DefaultLayout.js
+++ src/layout/DefaultLayout.js
@@ -1,5 +1,5 @@
 // @flow
-import React from 'react'
+import * as React from 'react'
 import { Route } from 'react-router'
 import Preloader from '../components/Preloader'
 import Header from './header/Header'

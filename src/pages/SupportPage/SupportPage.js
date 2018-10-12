4b502f438036c80872fcedaa1429ee02a91fab96
Alexey Ermolaev <dotterian@dotterian.ru>
2018-10-12 16:41:09 +0300

    Валидация в «поддержке» + политика конфиденциальности и условия использования

--- src/pages/SupportPage/SupportPage.js
+++ src/pages/SupportPage/SupportPage.js
@@ -2,6 +2,7 @@
 import React from 'react'
 import { view } from 'react-easy-state'
 import * as jsonpath from 'jsonpath'
+import validate from 'web-form-validator'
 import { i18n } from '../../state/i18n'
 import './SupportPage.scss'
 import Popup from '../../components/Popup/Popup'
@@ -13,7 +14,17 @@ type StateTypes = {
   headerSize: number,
   textSize: number,
   inputSize: number,
-  popup: boolean
+  popup: boolean,
+  formData: {
+    name: string,
+    email: string,
+    text: string
+  },
+  errors: {
+    name?: string,
+    email?: string,
+    text?: string
+  }
 }
 
 class SupportPage extends React.Component<PropTypes, StateTypes> {
@@ -22,7 +33,13 @@ class SupportPage extends React.Component<PropTypes, StateTypes> {
     headerSize: 116.25,
     textSize: 14,
     inputSize: 16,
-    popup: false
+    popup: false,
+    formData: {
+      name: '',
+      email: '',
+      text: ''
+    },
+    errors: {}
   }
   container: { current: null | HTMLDivElement } = React.createRef()
 
@@ -34,11 +51,18 @@ class SupportPage extends React.Component<PropTypes, StateTypes> {
 
   componentDidMount () {
     this.resizeText()
+    this.validate()
     window.addEventListener('resize', this.resizeText)
   }
 
   componentWillUnmount () {
     window.removeEventListener('resize', this.resizeText)
+  }
+
+  componentDidUpdate (prevProps: PropTypes, prevState: StateTypes) {
+    if (JSON.stringify(this.state.formData) !== JSON.stringify(prevState.formData)) {
+      this.validate()
+    }
   }
 
   resizeText = () => {
@@ -57,8 +81,50 @@ class SupportPage extends React.Component<PropTypes, StateTypes> {
     })
   }
 
-  submit = (event: SyntheticEvent<HTMLButtonElement>) => {
+  validate = () => {
+    const schema = {
+      name: [{
+        rule: 'minLength',
+        option: 2,
+        error: 'supportPage.email.errors.name_length'
+      }, {
+        rule: 'required',
+        error: 'supportPage.email.errors.name_required'
+      }],
+      email: [{
+        rule: 'email',
+        error: 'supportPage.email.errors.email_invalid'
+      }, {
+        rule: 'required',
+        error: 'supportPage.email.errors.email_required'
+      }],
+      text: [{
+        rule: 'minLength',
+        option: 10,
+        error: 'supportPage.email.errors.text_length'
+      }, {
+        rule: 'required',
+        error: 'supportPage.email.errors.text_required'
+      }]
+    }
+
+    const { errors, isValid } = validate(schema, this.state.formData)
+    this.setState({
+      errors: {
+        name: '',
+        email: '',
+        text: '',
+        ...errors
+      }
+    })
+    return isValid
+  }
+
+  submit = (event: SyntheticEvent<HTMLFormElement>) => {
     event.preventDefault()
+    if (!this.validate()) {
+      return
+    }
     this.setState({
       popup: true
     })
@@ -70,7 +136,8 @@ class SupportPage extends React.Component<PropTypes, StateTypes> {
       .email-container .section-name {
         font-size: ${this.state.headerSize}px;
       }
-      .email-container label {
+      .email-container label,
+      .email-container .error {
         font-size: ${this.state.textSize}px;
       }
       .email-container input,
@@ -116,16 +183,40 @@ class SupportPage extends React.Component<PropTypes, StateTypes> {
           <div className='row'>
             <div className='input-box'>
               <label>{i18n.t('supportPage.email.name')}*</label>
-              <input required />
+              <input onInput={(e: SyntheticEvent<HTMLInputElement>) => {
+                this.setState({
+                  formData: {
+                    ...this.state.formData,
+                    name: e.currentTarget.value
+                  }
+                })
+              }} className={this.state.errors.name ? 'has-error' : ''} />
+              {this.state.errors.name && <span className='error'>{i18n.t(this.state.errors.name)}</span>}
             </div>
             <div className='input-box'>
               <label>{i18n.t('supportPage.email.email')}*</label>
-              <input type='email' required />
+              <input onInput={(e: SyntheticEvent<HTMLInputElement>) => {
+                this.setState({
+                  formData: {
+                    ...this.state.formData,
+                    email: e.currentTarget.value
+                  }
+                })
+              }} className={this.state.errors.email ? 'has-error' : ''} />
+              {this.state.errors.email && <span className='error'>{i18n.t(this.state.errors.email)}</span>}
             </div>
           </div>
           <div className='row row-textarea'>
             <label>{i18n.t('supportPage.email.message')}</label>
-            <textarea />
+            <textarea onInput={(e: SyntheticEvent<HTMLTextAreaElement>) => {
+              this.setState({
+                formData: {
+                  ...this.state.formData,
+                  text: e.currentTarget.value
+                }
+              })
+            }} className={this.state.errors.text ? 'has-error' : ''} />
+            {this.state.errors.text && <span className='error'>{i18n.t(this.state.errors.text)}</span>}
           </div>
           <div className='row row-center'>
             <button type='submit' className='btn'>{i18n.t('supportPage.email.send')}</button>
